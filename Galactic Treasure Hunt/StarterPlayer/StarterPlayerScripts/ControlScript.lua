3264e49fb4d3f4c57c644efbffeffbe289c43a4c
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-05 05:17:46 +0800

    Update ControlScript.lua

--- Galactic Treasure Hunt/StarterPlayer/StarterPlayerScripts/ControlScript.lua
+++ Galactic Treasure Hunt/StarterPlayer/StarterPlayerScripts/ControlScript.lua
@@ -9,12 +9,13 @@ local player = game:GetService("Players").LocalPlayer
 -- Represents player's mouse
 local mouse = player:GetMouse()
 
--- Negative values indicate movement in the reverse direction along the Z-axis (forward direction)
-local PLAYER_SPEED = -48000
+local PLAYER_SPEED = 48000
 local FORWARD_KEY = Enum.KeyCode.W
+local SPACE_KEY = Enum.KeyCode.Space
 
 -- x-axis: left and right, y-axis: downward and upward, z-axis: forward and backward
-local forwardForceVector = Vector3.new(0, 0, PLAYER_SPEED)
+local forwardForceVector = Vector3.new(0, 0, -PLAYER_SPEED)
+local upForceVector = Vector3.new(0, PLAYER_SPEED, 0)
 
 -- Function for player movement
 local function onMove(actionName, inputState)
@@ -22,13 +23,15 @@ local function onMove(actionName, inputState)
     -- Check if the input key is pressed
     if inputState == Enum.UserInputState.Begin then
 
-        -- Apply forward force to move the player character
-        player.Character.HumanoidRootPart.VectorForce.Force = forwardForceVector
+        if actionName == "MoveForward" then
+            player.Character.HumanoidRootPart.VectorForce.Force = forwardForceVector
+        elseif actionName == "MoveUpward" then
+            player.Character.HumanoidRootPart.VectorForce.Force = upForceVector
+        end
 
-    -- When the key is released
     elseif inputState == Enum.UserInputState.End then
 
-        -- Stop applying force
+        -- Stop applying force when the key is released
         player.Character.HumanoidRootPart.VectorForce.Force = Vector3.new(0, 0, 0)
 
     end
@@ -55,5 +58,8 @@ end
 -- Bind the "Aim" action to the onAim function when there is mouse movement
 ContextActionService:BindAction("Aim", onAim, false, Enum.UserInputType.MouseMovement)
 
--- Bind the "Move" action to the onMove function triggered by "W" as its forward key
-ContextActionService:BindAction("Move", onMove, false, FORWARD_KEY)
+-- Bind the "MoveForward" action to the onMove function triggered by "W" as its forward key
+ContextActionService:BindAction("MoveForward", onMove, false, FORWARD_KEY)
+
+-- Bind the "MoveUpward" action to the onMove function triggered by spacebar
+ContextActionService:BindAction("MoveUpward", onMove, false, SPACE_KEY)
