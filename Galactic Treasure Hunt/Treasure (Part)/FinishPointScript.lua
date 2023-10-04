38907ba32f707f5f1fb04068522f882d5a5adeb7
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-04 20:49:06 +0800

    Create FinishPointScript.lua

---
+++ Galactic Treasure Hunt/Treasure (Part)/FinishPointScript.lua
@@ -0,0 +1,47 @@
+local timePassed = 0
+local treasure = script.Parent
+
+-- Function to finish the game
+function finish()
+
+    -- Make treasure disappear
+    treasure.Transparency = 1
+    treasure.CanCollide = false
+
+    print("You found the treasure in " .. timePassed)
+
+    if timePassed <= 15 then
+        print("You got a gold medal!")
+    elseif timePassed > 15 and timePassed <= 30 then
+        print("You got a silver medal!")
+    end
+
+end
+
+-- Function to handle when treasure is touched
+local function onTouch(otherPart)
+
+    local canHarvest = treasure:FindFirstChild("CanHarvest")
+
+    -- Check if treasure can be harvested
+    if canHarvest.Value == true then
+
+        if otherPart.Name == "HumanoidRootPart" or otherPart.Name == "Blaster" then
+            local character = otherPart.Parent
+            canHarvest.Value = false
+            finish()
+        end
+
+    end
+
+end
+
+-- Connect onTouch function to the Touched event of the Treasure object
+treasure.Touched:Connect(onTouch)
+
+while true do
+
+    wait(1)
+    timePassed += 1
+
+end
