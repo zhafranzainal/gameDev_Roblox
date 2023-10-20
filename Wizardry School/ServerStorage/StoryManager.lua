f578c0da544e97fb54d500e1369ff57aaeac74a3
Zhafran Zainal <73966883+zhafranzainal@users.noreply.github.com>
2023-10-20 16:17:30 +0800

    Add more dialogues

--- Wizardry School/ServerStorage/StoryManager.lua
+++ Wizardry School/ServerStorage/StoryManager.lua
@@ -11,8 +11,22 @@ local npcName = npcModel.Name
 
 -- Send signal as server
 local function task1()
+
     DialogueEvent:FireAllClients(npcImage, npcName, "Welcome to Wizardry School!")
     wait(5)
+
+    DialogueEvent:FireAllClients(npcImage, npcName, "I am Professor Wulfric.")
+    wait(3)
+
+    DialogueEvent:FireAllClients(npcImage, npcName, "Since it is your first day of school..")
+    wait(5)
+
+    DialogueEvent:FireAllClients(npcImage, npcName, ".. I will lead you to join the first class as an introduction.")
+    wait(6)
+
+    DialogueEvent:FireAllClients(npcImage, npcName, "Feel free to explore the castle while I'm preparing the class.")
+    wait(7)
+
 end
 
 function StoryManager.startStory()
